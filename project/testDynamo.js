0129dbddf77c747f4bf0713d609ebf4b8467d11e
Monishka Aswani <monishka.aswani@gmail.com>
2025-11-10 21:01:29 +0000

    New ui

--- project/testDynamo.js
+++
@@ -1,9 +0,0 @@
-// testDynamo.cjs
-const AWS = require('aws-sdk');
-
-const dynamodb = new AWS.DynamoDB({ region: 'eu-west-2' });
-
-dynamodb.listTables({}, (err, data) => {
-  if (err) console.error(err);
-  else console.log(data);
-});
